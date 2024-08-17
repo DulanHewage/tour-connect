11b099d8fcb4c328dc94313508aee8afdd58d921
Dulan Hewage <dulanhewage2@gmail.com>
2024-08-17 15:41:02 +0530

    Add comments in activity-service composable

--- nuxt-frontend/composables/activitiesService.ts
+++ nuxt-frontend/composables/activitiesService.ts
@@ -16,6 +16,10 @@ export const useActivityService = () => {
     specialOffer: false,
   });
 
+  /**
+   * Fetches activities based on the current search filters.
+   * @returns {Promise<{ activities: Ref<Activity[]>; pagination: Pagination | null; error: any; status: any; refresh: any }>}
+   */
   const getActivities = async () => {
     const { data, error, status, refresh } = await useAsyncData(
       "activity-grid",
@@ -38,6 +42,12 @@ export const useActivityService = () => {
 
     return { activities, pagination, error, status, refresh };
   };
+
+  /**
+   * Fetches an activity by its ID.
+   * @param {string} id - The ID of the activity to fetch.
+   * @returns {Promise<Activity | undefined>} The activity if found, otherwise undefined.
+   */
   const getActivityById = async (id: string): Promise<Activity | undefined> => {
     let activity = activities.value.find(
       (activity: Activity) => activity._id === id
@@ -56,10 +66,15 @@ export const useActivityService = () => {
     }
     return activity;
   };
+
+  /**
+   * Debounced function to fetch activities.
+   */
   const debouncedGetActivities = useDebounceFn(async () => {
     await getActivities();
   }, 500);
 
+  // Watch for changes in search filters and fetch activities accordingly
   watch(searchFilters.value, async () => {
     await debouncedGetActivities();
   });
