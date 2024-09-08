d0f6918bf44aa918f2354d8a55209777e4c545ff
Dulan Hewage <dulanhewage2@gmail.com>
2024-09-08 16:35:35 +0530

    improve the accessibility of the navigation

--- nuxt-frontend/components/NavBar.vue
+++ nuxt-frontend/components/NavBar.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="bg-sky-500 h-16 items-center flex">
-    <nav class="flex justify-between w-full container">
+    <nav
+      class="flex justify-between w-full container"
+      aria-label="Main Navigation"
+    >
       <NuxtLink class="text-lg text-white font-bold" to="/"
         >Tour Connect</NuxtLink
       >
       <div class="flex gap-2 *:text-white">
-        <NuxtLink to="/" :active-class="'underline'">Home</NuxtLink>
-        <NuxtLink to="/about" :active-class="'underline'">About</NuxtLink>
+        <NuxtLink to="/" :active-class="'underline'" aria-label="Home"
+          >Home</NuxtLink
+        >
+        <NuxtLink to="/about" :active-class="'underline'" aria-label="About"
+          >About</NuxtLink
+        >
       </div>
     </nav>
   </div>
